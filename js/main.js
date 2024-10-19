1517f71087d74f4162a6f11fed1c747b395932b5
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2024-10-19 20:12:10 -0300

    template-update

--- js/main.js
+++ js/main.js
@@ -14,16 +14,6 @@
     
     // Initiate the wowjs
     new WOW().init();
-
-
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 90) {
-            $('.nav-bar').addClass('sticky-top shadow');
-        } else {
-            $('.nav-bar').removeClass('sticky-top shadow');
-        }
-    });
     
     
     // Dropdown on mouse hover
